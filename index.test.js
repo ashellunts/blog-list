224232051faa66431234961d00e1a70daed9d2eb
Artur Shellunts <artur.shellunts@profiforms.de>
2022-11-08 09:59:20 +0100

    add describe block in the test

--- index.test.js
+++ index.test.js
@@ -7,29 +7,31 @@ const blog = {
     likes: 123
 }
 
-test('get all blogs', async () => {
-    const response = await axios.get('http://localhost:3003/api/blogs')
-    expect(response.status).toBe(200)
+describe('all tests', () => {
+    test('get all blogs', async () => {
+        const response = await axios.get('http://localhost:3003/api/blogs')
+        expect(response.status).toBe(200)
 
-    const firstBlog = response.data[0]
-    expect(blog).toEqual({
-        title: firstBlog.title,
-        author: firstBlog.author,
-        url: firstBlog.url,
-        likes: firstBlog.likes,
+        const firstBlog = response.data[0]
+        expect(blog).toEqual({
+            title: firstBlog.title,
+            author: firstBlog.author,
+            url: firstBlog.url,
+            likes: firstBlog.likes,
+        })
     })
-})
 
-test('create blog', async () => {
-    const response = await axios.post('http://localhost:3003/api/blogs', blog)
-    expect(response.status).toBe(201)
+    test('create blog', async () => {
+        const response = await axios.post('http://localhost:3003/api/blogs', blog)
+        expect(response.status).toBe(201)
 
-    const blogCreated = response.data
-    expect(blogCreated._id).toBeDefined()
-    expect(blog).toEqual({
-        title: blogCreated.title,
-        author: blogCreated.author,
-        url: blogCreated.url,
-        likes: blogCreated.likes,
+        const blogCreated = response.data
+        expect(blogCreated._id).toBeDefined()
+        expect(blog).toEqual({
+            title: blogCreated.title,
+            author: blogCreated.author,
+            url: blogCreated.url,
+            likes: blogCreated.likes,
+        })
     })
 })
