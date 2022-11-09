74f7cb5164e01454ffec23934843400de0f0dd7d
Artur Shellunts <artur.shellunts@profiforms.de>
2022-11-09 09:24:24 +0100

    simplify mostBlogs with lodash

--- utils/list_helpers.js
+++ utils/list_helpers.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 function totalLikes(blogList) {
     const reducer = (prevValue, curValue) => prevValue + curValue.likes
 
@@ -20,17 +22,12 @@ function mostBlogs(blogList) {
     if (blogList.length == 0) {
         return null
     }
-    const blogsByAuthors = {}
     const mostBlogsAuthor = { author: blogList[0].author, blogs: 1 }
-    for (const blog of blogList) {
-        if (blogsByAuthors[blog.author] === undefined) {
-            blogsByAuthors[blog.author] = 1
-        } else {
-            blogsByAuthors[blog.author] += 1
-        }
-        if (mostBlogsAuthor.blogs < blogsByAuthors[blog.author]) {
-            mostBlogsAuthor.author = blog.author
-            mostBlogsAuthor.blogs = blogsByAuthors[blog.author]
+    const blogsCountByAuthor = _.countBy(blogList, 'author')
+    for (const [author, blogs] of Object.entries(blogsCountByAuthor)) {
+        if (mostBlogsAuthor.blogs < blogs) {
+            mostBlogsAuthor.author = author
+            mostBlogsAuthor.blogs = blogs
         }
     }
     return mostBlogsAuthor
